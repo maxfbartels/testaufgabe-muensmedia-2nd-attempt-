6eb522173342bd501258973f64aae2034c0a077d
maxfbartels <maxfbartels@gmail.com>
2026-04-23 15:43:46 +0000

    added code to check if  the  space is free and put a circle in if so

--- app/app/App/Http/Controllers/GameController.php
+++ app/app/App/Http/Controllers/GameController.php
@@ -175,12 +175,23 @@ class GameController extends Controller
         // Once all the checks have passed, you can finally update the game board by calling
         // $game->setSpace( $x, $y, GameMark::Circle ).
         // [ The code to check if the space is free goes here ]
+        $content = $game->getSpace( $x, $y );
+            if ($content === GameMark::None){
+
+                $game->setSpace( $x, $y, GameMark::Circle );
+            }
+            else {
+
+                return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
+            }
+            
+
 
         // If the space is not free, run the code in the line below by removing the //
         //return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
 
         // [ The code to update the game board goes here ]
-
+    //already implemented above :)
         // Saving the game board and output it to the player
         $game->save();
         return $this->status_output( $game );
